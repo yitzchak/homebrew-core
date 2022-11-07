43b8f5fbaa298e6a7e218fe36884d8a6be4c8bfe
Tarn W. Burton <twburton@gmail.com>
2022-11-07 06:38:29 -0500

    clasp-cl 2.2.0 (new formula)

---
+++ Formula/clasp-cl.rb
@@ -0,0 +1,35 @@
+class ClaspCl < Formula
+  desc "Common Lisp implementation that brings Common Lisp and C++ together"
+  homepage "https://github.com/clasp-developers/clasp"
+  url "https://github.com/clasp-developers/clasp/archive/refs/tags/2.2.0.tar.gz"
+  sha256 "b0535e388bd25c72a7290def740b620870288b2d5f6e8135c79870720eb08eb2"
+  license "GPL-2.0-or-later"
+  head "https://github.com/clasp-developers/clasp.git", branch: "main"
+
+  depends_on "boost" => :build
+  depends_on "libunwind-headers" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sbcl" => :build
+  depends_on arch: :x86_64
+  depends_on "fmt"
+  depends_on "gmp"
+  depends_on "llvm@15"
+
+  def install
+    ENV.deparallelize
+    system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
+      "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@15"].opt_bin}/llvm-config",
+      "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
+      "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
+      "--skip-sync=ansi-test,mps,cl-bench,cl-who"
+    system "ninja", "-C", "build", "install"
+  end
+
+  test do
+    (testpath/"simple.lisp").write <<~EOS
+      (write-line "Hello, world!")
+    EOS
+    assert_match "Hello, world!", shell_output("#{bin}/clasp --script #{testpath}/simple.lisp")
+  end
+end
